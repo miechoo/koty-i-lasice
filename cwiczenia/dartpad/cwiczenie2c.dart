52270a0abe93d2aae0cc345e13ad65e5a51d3eb3
Pawel Miechu <pawel.miechowiecki@gmail.com>
2023-10-24 23:44:36 +0200

    ćwiczenie 2, klasa i obiekty

--- cwiczenia/dartpad/cwiczenie2c.dart
+++ cwiczenia/dartpad/cwiczenie2c.dart
@@ -17,28 +17,29 @@ void main() {
   kot3.przedstawSie();
 }
 
-// klasa Kot ogólnie opisuje, z czego kot się składa i co robi
+// Klasa Kot ogólnie opisuje, z czego kot się składa i co robi
 class Kot {
-  // kot składa się z imienia
+  // Kot składa się z imienia
   String imie = '';
 
-  // imię kotu jest nadawane tutaj - to specjana funkcja, konstruktor.
+  // Imię kotu jest nadawane tutaj - to specjana funkcja, konstruktor.
   // Funkcja ta ma taką samą nazwę jak nazwa klasy.
   // Konstruktor przyjmuje jeden parametr - inicjalne imię kota. Na przykład Kot('Futrzak').
   Kot(String inicjalneImie) {
     imie = inicjalneImie;
   }
 
-  // procedura ta jest publiczną metodą klasy Kot.
+  // Procedura ta jest publiczną metodą klasy Kot.
   // Wyświetla napis i wykonuje kolejną procedurę _przedstawSieOgolnie()
   void przedstawSie() {
     print('Mrrrrrr.... jestem $imie');
     _przedstawSieOgolnie();
   }
 
-  // Procedura ta jest procedurą klasy Kot. Wyświetla napis.
-  // Prywatna procedura może być użyta tylko z innej procedury.
+  // Procedura ta jest prywatną procedurą klasy Kot. Wyświetla napis.
+  // Prywatna procedura może być użyta tylko z innej procedury (metody).
   // Nie można na przykład wykonać kot3._przedstawSieOgolnie().
+  // W języku Dart, żeby metoda była prywatna, musi zaczynać się od znaku podkreślenia _
   void _przedstawSieOgolnie() {
     print('Mrrrrrr.... jestem kocurem');
   }
