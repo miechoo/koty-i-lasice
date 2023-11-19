99c33c226aa27bd6bd2ab154b32e6e265c905421
Pawel Miechu <pawel.miechowiecki@gmail.com>
2023-11-19 11:46:16 +0100

    literówki i inne

--- cwiczenia/dartpad/cwiczenie3.dart
+++ cwiczenia/dartpad/cwiczenie3.dart
@@ -1,6 +1,6 @@
 // to jest gra
 
-// niestety nie da sie jej uruchomić na stronie darpard.dev, bo ta strona
+// niestety nie da sie jej uruchomić na stronie dartpad.dev, bo ta strona
 // nie obsługuje wprowadzania danych do gry w trakcie gry
 
 import 'dart:math';
@@ -8,16 +8,16 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   Gra gra = Gra();
-  gra.uruchom();
+  gra.uruchomGre();
 }
 
 class Gra {
-  static const int ilePartii = 3;
+  static const int _ilePartii = 3;
   int _wynikGry = 0;
-  void uruchom() {
-    for (int i = 0; i < 3; i++) {
+  void uruchomGre() {
+    for (int i = 0; i < _ilePartii; i++) {
       Partia aktualnaPartia = Partia();
-      _wynikGry += aktualnaPartia.uruchom();
+      _wynikGry += aktualnaPartia.uruchomPartie();
     }
     print('Ukończyłeś grę z wynikiem $_wynikGry. Gratulacje!');
     print('Naciśnij dowolny klawisz żeby zakończyć.');
@@ -28,25 +28,32 @@ class Gra {
 class Partia {
   late final int _tajemniczaLiczba;
   int _liczbaProb = 1;
-  bool czyZgadl = false;
-  int uruchom() {
+  bool _czyZgadl = false;
+
+  int uruchomPartie() {
     print('------nowa partia----------');
+
     _tajemniczaLiczba = Random().nextInt(10);
-    while (!czyZgadl) {
+
+    while (!_czyZgadl) {
       print('Mam na myśli jakąś liczbę w przedziale od 0 do 9');
       print('Czy gadniesz jaką?');
+
       stdout.write('Wpisz liczbę i naciśnij enter: ');
       String wprowadzonyNapis = stdin.readLineSync() ?? '';
+
       if (_tajemniczaLiczba == int.parse(wprowadzonyNapis)) {
         print(
-            'Brawo, chodziło o liczbę $_tajemniczaLiczba. Zgadłeś w $_liczbaProb liczbie prób.');
-        czyZgadl = true;
+            'Brawo, chodziło o liczbę $_tajemniczaLiczba. Zgadłeś w $_liczbaProb próbach.');
+        _czyZgadl = true;
       } else if (_tajemniczaLiczba > int.parse(wprowadzonyNapis)) {
         print('Podana przez Ciebie liczba jest za mała. Spróbuj jeszcze raz.');
         _liczbaProb++;
+        _czyZgadl = false;
       } else {
         print('Podana przez Ciebie liczba jest za duża. Spróbuj jeszcze raz.');
         _liczbaProb++;
+        _czyZgadl = false;
       }
     }
     return _liczbaProb;
